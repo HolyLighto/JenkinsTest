4310d652ef1d48bee72f63598ccf22a3f4a415aa
HolyLighto <44924682+HolyLighto@users.noreply.github.com>
2021-11-02 12:02:35 +0300

    Add project files.

---
+++ JenkinsTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace JenkinsTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1,1, "Nums are not equals");
+        }
+    }
+}
