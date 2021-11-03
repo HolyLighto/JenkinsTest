5c2da83d4398ad8a21bc25a197660ebe7a672e1e
HolyLighto <44924682+HolyLighto@users.noreply.github.com>
2021-11-03 17:54:00 +0300

    settings

--- JenkinsTest/UnitTest1.cs
+++ JenkinsTest/UnitTest1.cs
@@ -1,14 +1,18 @@
+using Aquality.Selenium.Browsers;
 using NUnit.Framework;
 
 namespace JenkinsTest
 {
     public class Tests
     {
+        private Browser _browser;
         [SetUp]
         public void Setup()
         {
+            _browser = AqualityServices.Browser;
         }
 
+        [Test]
         public void Test1()
         {
             Assert.AreEqual(1, 1, "Nums are not equal");
