24466903fe6d3c47870be6e21a3a3bbffcdd3af2
HolyLighto <44924682+HolyLighto@users.noreply.github.com>
2021-11-03 15:48:15 +0300

    second test added
    1

--- JenkinsTest/UnitTest1.cs
+++ JenkinsTest/UnitTest1.cs
@@ -12,8 +12,14 @@ namespace JenkinsTest
         [Test]
         public void Test1()
         {
-            Assert.AreEqual(1,1, "Nums are not equals");
-            Assert.AreEqual(0, 1, "Nums are not equals");
+            Assert.AreEqual(1, 1, "Nums are not equal");
+            Assert.AreEqual(0, 1, "Nums are not equal");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(11, 11, "Test2 Nums are not equal");
         }
     }
 }
